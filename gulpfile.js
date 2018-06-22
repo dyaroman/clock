64ad0358f8e3b6827b0a1c67d6e8bbb22de3f9b2
Roman Dyachenko <roman@leadsmarket.com>
2018-06-22 15:17:22 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+const puppeteer = require('puppeteer');
+
+const htmlTask = require('./gulp-tasks/html');
+const stylesTask = require('./gulp-tasks/styles');
+const jsTask = require('./gulp-tasks/scripts');
+const imagesTask = require('./gulp-tasks/images');
+
+gulp.task('html', () => {
+  console.log(`build html`);
+  return htmlTask.html();
+});
+
+gulp.task('css', () => {
+  console.log(`build css`);
+  return stylesTask.styles();
+});
+
+gulp.task('js', () => {
+  console.log(`build js`);
+  jsTask.jsLibs();
+  return jsTask.scripts();
+});
+
+gulp.task('images', () => {
+  console.log(`build images`);
+  imagesTask.favicons();
+  return imagesTask.images();
+});
+
+gulp.task('server', () => {
+  connect.server({
+    root: `./dest/`,
+    livereload: false
+  });
+});
+
+gulp.task('watch', () => {
+  htmlTask.htmlWatcher();
+  stylesTask.cssWatcher();
+  jsTask.jsWatcher();
+  imagesTask.imagesWatcher();
+  gulp.task('server')();
+});
+
+gulp.task('pdf', () => {
+  gulp.task('server')();
+
+  const exit = process.exit;
+
+  (async () => {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const page = await browser.newPage();
+    await page.goto(`http://localhost:8080`, {
+      waitUntil: 'networkidle2'
+    });
+    await page.emulateMedia('print');
+    await page.pdf({
+      path: './dest/cv.pdf',
+      format: 'A4'
+    });
+    await exit();
+    await browser.close();
+  })();
+});
+
+gulp.task('build', (cb) => {
+  gulp.task('html')();
+  gulp.task('css')();
+  gulp.task('js')();
+  gulp.task('images')();
+  gulp.task('pdf')();
+
+  cb();
+});
